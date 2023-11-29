24720e55f5c6ab27e3152f153a397bbe4fb5c0d1
Kateřina Pleskačová <katerina.pleskacova91@gmail.com>
2023-11-29 06:50:52 +0100

    create JS for dialog on login and register html templates

---
+++ script/script.js
@@ -0,0 +1,28 @@
+function handleDialogOpen () {
+   const openDialogElements = document.getElementsByClassName('open-dialog');
+
+      if(openDialogElements.length > 0) {
+         for(const element of openDialogElements) {
+            const dialogId = element.dataset.dialog;
+            const dialog = document.getElementById(dialogId);
+
+            if (dialog) {
+               const closeButton = dialog.querySelector('.close-dialog')
+               element.addEventListener('click', () => {
+                  dialog.showModal();
+               });
+
+               if (closeButton) {
+                  closeButton.addEventListener('click', () => {
+                     dialog.close();
+                  });
+               }
+            }
+         }
+      }
+}
+
+// Počkáme na dokončení konstrukce DOMu, abychom funkci volali, až bude celý Document object model načtený
+document.addEventListener('DOMContentLoaded', () => {
+   handleDialogOpen();
+});
