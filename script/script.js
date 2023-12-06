d24390e375bc79fe376c4ee8abc6b16cfa3a935a
Kateřina Pleskačová <katerina.pleskacova91@gmail.com>
2023-12-06 10:00:43 +0100

    create JS for toasts and create style for toasts in css file

--- script/script.js
+++ script/script.js
@@ -23,6 +23,62 @@ function handleDialogOpen () {
       }
 }
 
+// Vyskakovací notifikační okna - Toasty
+
+function removeToast(toastId) {
+   const toast = document.getElementById(toastId);
+   if (toast) {
+      toast.remove();
+   }
+}
+
+function removeAllToasts () {
+   const toasts = document.getElementsByClassName('toast');
+   for (let toast of toasts) {
+      toast.remove ();
+   }
+}
+function createToast(message, delay = 3000) {
+   const toastId = `toast-${Math.random()}`;
+
+   const toast = document.createElement('div');
+
+   toast.setAttribute('id', toastId);
+   toast.classList.add('toast');
+
+   const em = document.createElement('em');
+   em.innerText = message;
+
+   toast.appendChild(em);
+
+   document.body.appendChild(toast);
+
+   setTimeout(() => {
+      removeToast(toastId);
+   }, delay);
+
+   return toastId;
+}
+
+
+
+const toastId = createToast('Testovací zpráva', 10000);
+
+//Validace formulářů
+function validateForm(formName) {
+   const formChildren = document.forms[formName].children;
+
+   if (formChildren.length > 0) {
+      for (const formElement of formChildren) {
+         if (formElement.name) {
+            console.log(formElement);
+         }
+      }
+   }
+
+   return false;
+}
+
 // Počkáme na dokončení konstrukce DOMu, abychom funkci volali, až bude celý Document object model načtený
 document.addEventListener('DOMContentLoaded', () => {
    handleDialogOpen();
