ac431fe1521e0a7f1d5c9c0925156d256b19c453
Kateřina Pleskačová <katerina.pleskacova91@gmail.com>
2023-12-09 07:07:55 +0100

    finish form validation for login and register

--- script/script.js
+++ script/script.js
@@ -61,22 +61,71 @@ function createToast(message, delay = 3000) {
 }
 
 
-
-const toastId = createToast('Testovací zpráva', 10000);
-
 //Validace formulářů
+
+function validateInput(input) {
+   // Jak zjistit název tagu
+   // if (input.localName === 'textarea' && input.value.length === 0) {
+   //    return false;
+   // }
+
+   if (input.dataset.compare) {
+      const inputToCompare = document.getElementById(input.dataset.compare);
+         if (inputToCompare) {
+            return {
+               result: input.value === inputToCompare.value,
+               message: input.dataset.message,
+            }
+         }
+   }
+
+   switch (input.type) {
+      case 'email':
+         return {
+            result: /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/gi.test(input.value),
+            message: 'Zadejte validní email.' ,
+         };
+      case 'password':
+         return {
+            result: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm.test(input.value),
+            message: 'Heslo musí obsahovat alespoň jednu číslici, velké a malé písmeno a musí mít nejméně 8 znaků.'
+         };
+      case 'checkbox':
+         return {
+            result: input.checked,
+            message: 'Pro dokončení registrace musíte zaškrtnout pole "Souhlasím s podmínkami použití"'
+         }
+      default:
+         return {
+            result: 'input.value.length !== 0' ,
+            message: 'Pole nesmí být prázdné.',
+         }
+   }
+}
 function validateForm(formName) {
    const formChildren = document.forms[formName].children;
-
+   let isValid = true;
    if (formChildren.length > 0) {
-      for (const formElement of formChildren) {
-         if (formElement.name) {
-            console.log(formElement);
+      for (let formElement of formChildren) {
+         if (formElement.classList.contains('checkbox-label')) {
+            const checkbox = formElement.getElementsByTagName('input');
+            if (checkbox.length) {
+               formElement = checkbox[0];
+            }
+         }
+
+         if (formElement.name && formElement.required) {
+            const validationResult = validateInput(formElement);
+            isValid = validationResult.result && isValid;
+
+            if (!validationResult.result) {
+               createToast(validationResult.message, 10000);
+            }
          }
       }
    }
 
-   return false;
+   return isValid;
 }
 
 // Počkáme na dokončení konstrukce DOMu, abychom funkci volali, až bude celý Document object model načtený
