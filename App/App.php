549c3deede91bb4891e42a09db9207badb362a25
Kateřina Pleskačová <katerina.pleskacova91@gmail.com>
2023-12-17 09:12:07 +0100

    Feat: create .env parser and use it in App constructor

--- App/App.php
+++ App/App.php
@@ -3,7 +3,23 @@
 declare(strict_types=1);
 
 namespace App;
+
+use App\AppCore\Exceptions\EnvFileNotFoundException;
+use App\AppCore\Utils\EnvParser;
+
 class App {
+    public function __construct()
+    {
+        session_start();
+
+        try {
+            EnvParser::parse("../.env");
+        } catch(EnvFileNotFoundException $e) {
+            echo "Chyba aplikace...";
+            die();
+        }
+    }
+
     public function run(): void
     {
         echo "Aplikace běží...";
