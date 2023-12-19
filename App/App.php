f6b0e93319fe62e216b629b0715157b9e32bab00
Kateřina Pleskačová <katerina.pleskacova91@gmail.com>
2023-12-19 07:18:46 +0100

    Feat: update App.php

--- App/App.php
+++ App/App.php
@@ -5,26 +5,54 @@ declare(strict_types=1);
 namespace App;
 
 use App\AppCore\Exceptions\EnvFileNotFoundException;
+use App\AppCore\Exceptions\RouteNotFoundException;
+use App\AppCore\Routing\Router;
 use App\AppCore\Utils\EnvParser;
+use App\AppCore\View\View;
+use App\Controllers\RegisterController;
+use App\Routes\Routes;
+use Closure;
+use App\Controllers\HomepageController;
 
 class App {
+
+    private Router $router;
     public function __construct()
     {
         session_start();
-        define('__APP_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/');
 
         try {
             EnvParser::parse("../.env");
         } catch(EnvFileNotFoundException $e) {
-            //TODO: nahradit lepším zpracováním chyby
+            //TODO: zobrazit správnou šablonu
             echo "Chyba aplikace...";
             die();
         }
+
+        define('__APP_ROOT__', $_SERVER['DOCUMENT_ROOT'] . '/');
+        define('__APP_RESOURCES__', getenv('APP_DOMAIN') . getenv('APP_SUB_FOLDERS') . 'resources/');
+
+        $this->router = new Router();
+    }
+
+    private function registerRoutes(): void
+    {
+        $this->router->get(Routes::Homepage, [HomepageController::class, 'index']);
+
+        $this->router
+            ->get(Routes::Register, [RegisterController::class, 'create'])
+            ->post(Routes::Register, [RegisterController::class, 'store']);
     }
 
     public function run(): void
     {
-        //TODO: doopravdy spustit aplikaci
-        echo "Aplikace běží...";
+        try {
+            $this->registerRoutes();
+            $this->router->resolveRequest();
+        } catch (RouteNotFoundException $e) {
+            http_response_code(404);
+            // TODO: zobrazit správnou šablonu
+            echo "Stránka nenalezena!";
+        }
     }
 }
